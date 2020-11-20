1444ebaac19d76dc32a98e7d3d709504202f30ed
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-11-20 15:16:33 +0100

    Removed default files

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <appheader> 
+    </appheader>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    appheader: Header,
   }
 }
 </script>
